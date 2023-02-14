c42180b5cb76653cd6736dcec372d2708bbefdce
abenricks <abenricks@gmail.com>
2023-02-13 20:09:11 -0500

    Deleted the original file content for the react app page, added some folders for pages and components, set up the basics for routing around the app and selecting different pages, tried some CSS editing for the main page.

--- cinema-booking-client/src/App.js
+++ cinema-booking-client/src/App.js
@@ -1,27 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, Routes} from 'react-router-dom';
+import Navbar from './components/Navbar/NavbarElements';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p> Austin Barfield </p>
-        <p> Nidhi Dahiya </p>
-        <p>Aben Ricks</p>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+
+      </Routes>
+    </Router>
+  
   );
 }
 
---
+++ cinema-booking-client/src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+    return (
+        <div className="homepg">
+            Main Page of the website, will include the carousel and movie listings.
+        </div>
+    );
+};
+
+export default Home;
