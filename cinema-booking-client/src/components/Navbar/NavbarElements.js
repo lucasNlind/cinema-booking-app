ae78c028348fa0756746d3ed9cbefa3f009645fc
abenricks <abenricks@gmail.com>
2023-03-18 12:29:43 -0400

    Changed the color palette to see if it would look better, changed fonts to be uniform, added the logo to the main navigation bar and the website tab. Did small ui fixes for buttons and tables.

--- cinema-booking-client/src/components/Navbar/NavbarElements.js
+++ cinema-booking-client/src/components/Navbar/NavbarElements.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import './navbarElements.css';
+import logo from "../../images/logo-design-color.png";
 
 
 const NavbarElements= () =>{
     return (
-      <ul className="navflex">
-          <li>
-              <Link to="/">Home</Link>
-          </li>
-          <li>
-              <Link to="/login">Login</Link>
-          </li>
-          <li>
-              <Link to="/register">Register</Link>
-          </li>
-          <li>
-              <Link to="/adminhome">Admin</Link>
-          </li>
-      </ul>
+        <div className="sticky-bar">
+            
+            <ul className="navflex">
+                <img className="nav-logo" src={logo} alt="logo"/>
+                <h1 className="nav-title">C3 Cinemas</h1>
+                {/**This is an invisible divider so the logos are on one side and the links are on the other */}
+                <h1 className='divider'>  </h1>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/login">Login</Link>
+            </li>
+            <li>
+                <Link to="/register">Register</Link>
+            </li>
+            <li>
+                <Link to="/adminhome">Admin</Link>
+            </li>
+            </ul>
+        </div>
   );
 }
 export default NavbarElements;
