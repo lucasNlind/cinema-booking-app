091d2ca634d32d126b3c54d042518c3755abee15
abenricks <abenricks@gmail.com>
2023-03-20 18:40:09 -0400

    Added the fields and buttons needed for profile for deliverable 6

--- cinema-booking-client/src/pages/login/ChangePassword.js
+++ cinema-booking-client/src/pages/login/ChangePassword.js
@@ -1,10 +1,16 @@
 import './changePassword.css';
+import {Link} from 'react-router-dom';
 
 //Change password to something new in the DB. Needs a button to return to the edit page
 const ChangePassword = () => {
     return (
     <div className="change-pw-wrap">
         <form>
+
+        <div className="password-container">
+           <label>Enter your Current Password</label>
+            <input type="password" name="currentPass" required /> 
+        </div>
         
         <div className="password-container">
             <label>New Password</label>
@@ -15,8 +21,9 @@ const ChangePassword = () => {
             <label>Type Password Again</label>
             <input type="password" name="resetPassCheck" required />
         </div>
-       
+
         </form>
+        <Link className="change-pw-btn" to="/profile">Update Password</Link>
     </div>
     );
 };
